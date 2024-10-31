705c91e724d8694e7ef7438f3512565f2f5a00c2
Vladimir Ermak <VovchikShow.vs@gmail.com>
2024-10-31 17:14:27 +0300

    Update Pz3.py

--- Практическое занятие 3/Pz3.py
+++ Практическое занятие 3/Pz3.py
@@ -1,5 +1,6 @@
 from ConsistentFunc import CoderFunc as coder
-from ShannonFanoFunc import ShannonFano as Fano 
+from ShannonFanoFunc import ShannonFano as Fano
+from prettytable import PrettyTable
 
 # Исходная фраза
 phrase = 'Информация и информационные технологии в технических системах'
@@ -18,3 +19,15 @@ print(f'Исходная строка: {phrase}\n\nРезультат:', coder.e
 decode_phrase = coder.decoder(phrase, fano.syms)
 print('\nДекодированная строка:', decode_phrase)
 print('Сравнение строк:', decode_phrase == phrase)
+
+print('\n---\n')
+# Создание таблицы вероятности появления каждой из букв
+# Указываем заголовки страницы
+head = head = ['Буква', 'Вероятность', 'Бин код']
+# Создаём объект таблицы
+table = PrettyTable(head)
+for sym in fano.syms:
+    # Сборка таблицы
+    table.add_row([sym.key, sym.ver, sym.bincode])
+
+print(table)
